2059340e50f6faf2f9246d12e0c5125a1e289c06
12haa <mkhaefi2500@yahoo.com>
2025-12-02 17:20:33 +0330

    change tittle

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
       <header className="mb-8">
-        <h1 className="text-3xl font-bold text-gray-800">🚀 کاربران برنامه</h1>
-        <p className="text-gray-600 mt-2">Updated Title</p>
+        <h1 className="text-3xl font-bold text-gray-800">🚀ما کاربران برنامه</h1>
+        <p className="text-gray-600 mt-2">123456</p>
       </header>
 
       <UserList />
