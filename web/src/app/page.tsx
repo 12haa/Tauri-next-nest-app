034c4ee33301b6b1047328e72a93eca08da0868e
12haa <mkhaefi2500@yahoo.com>
2025-12-02 09:24:01 +0330

    change test88

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
       <header className="mb-8">
-        <h1 className="text-3xl font-bold text-gray-800">🚀 1289037123 کاربران</h1>
+        <h1 className="text-3xl font-bold text-gray-800">🚀 8888 کاربران</h1>
         <p className="text-gray-600 mt-2">اپلیکیشن asdasd شده با Tauri، Next.js و NestJS</p>
       </header>
 
