ff6c5ec89b00ea2b60f764471e0076ad23ba0588
12haa <mkhaefi2500@yahoo.com>
2025-12-02 15:33:03 +0330

    chnaged workflow file v5

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
       <header className="mb-8">
-        <h1 className="text-3xl font-bold text-gray-800">🚀 MAMAD کاربران</h1>
+        <h1 className="text-3xl font-bold text-gray-800">🚀 22222MAMAD کاربران</h1>
         <p className="text-gray-600 mt-2">test test test update Tauri، Next.js و NestJS</p>
       </header>
 
