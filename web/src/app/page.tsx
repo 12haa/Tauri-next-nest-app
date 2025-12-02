6292ed2e639baa6bc01aebc6a0f361c439010d86
12haa <mkhaefi2500@yahoo.com>
2025-12-02 16:59:11 +0330

    change tiitle in page.tsx

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
       <header className="mb-8">
-        <h1 className="text-3xl font-bold text-gray-800">🚀 mamad 14 fff کاربران</h1>
-        <p className="text-gray-600 mt-2">test test test update Tauri، Next.js و NestJS</p>
+        <h1 className="text-3xl font-bold text-gray-800">🚀 کاربران برنامه</h1>
+        <p className="text-gray-600 mt-2">Updated Title</p>
       </header>
 
       <UserList />
