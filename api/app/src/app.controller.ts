7d6d8a04c6192eec76f1ffe1c26b15428a6575c4
Mohamad Khaefi <mkhaefi.work@gmail.com>
2025-12-06 04:27:08 -0800

    feat: implement sidecar architecture and fix migrations logic

--- api/app/src/app.controller.ts
+++ api/app/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('tables')
+  async getTables() {
+    return this.appService.getTables();
+  }
 }
